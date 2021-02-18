c7ce126e51f273d36f51d03ef303fbd582bd7023
hp <hashparty@protonmail.com>
2021-02-18 12:43:47 -0800

    ...

--- src/buildList.js
+++ src/buildList.js
@@ -1,6 +1,6 @@
 const { version } = require('../package.json');
 const mainnet = require('./tokens/mainnet.json');
-const testnet = require('./tokens/testnet.json');
+// const testnet = require('./tokens/testnet.json');
 
 module.exports = function buildList() {
   const parsed = version.split('.');
@@ -20,7 +20,7 @@ module.exports = function buildList() {
     ],
     tokens: [
       ...mainnet,
-      ...testnet
+      // ...testnet
     ]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
